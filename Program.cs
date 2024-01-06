6ed9a06d7c557f4061080c517f691a0eb976aa46
Katsiaryna007 <katsiaryna.4job@gmail.com>
2024-01-06 09:25:22 +0300

    Программа

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+string[] newArray = new string[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine("Новый массив: ");
+for (int j = 0; j < count; j++) Console.WriteLine(newArray[j]);
